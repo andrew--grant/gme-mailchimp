8e6da196761e2ec43340c764d83d0a57f112fc58
Andrew Grant <agrant@gme.net.au>
2016-02-12 14:44:22 +1100

    deploy and test duplicate email logic -take 2

--- routes/subscriptions.js
+++ routes/subscriptions.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var Mailchimp = require('mailchimp-api-v3');
-var mailchimpUrl = 'http://us10.api.mailchimp.com';//  'http://us9.api.mailchimp.com';
-var apiKey = 'e3e446cfa981120859f63bef031cc0ec-us10'; // 'd497aacaafd66952fd2121fb8799cee4-us9';
-var listId = 'ee321b4c68'; // '5a388fe274';
+var mailchimpUrl = 'http://us9.api.mailchimp.com';//'http://us10.api.mailchimp.com';
+var apiKey = 'd497aacaafd66952fd2121fb8799cee4-us9'; // 'e3e446cfa981120859f63bef031cc0ec-us10';
+var listId = '5a388fe274';// 'ee321b4c68';
 var mailchimp = new Mailchimp(apiKey);
 var request = require('request');
 
