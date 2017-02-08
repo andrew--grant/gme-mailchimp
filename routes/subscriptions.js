6b36fbdc0f85616ad1ef1241ab0d7fbd3ded8e3d
Andrew Grant <agrant@gme.net.au>
2017-02-09 10:57:41 +1100

    trying out mailchimp data inserts

--- routes/subscriptions.js
+++ routes/subscriptions.js
@@ -24,7 +24,6 @@ router.get('/', function (req, res, next) {
 });
 
 
-
 // POST subscriptions/compsubscribe
 router.post('/compsubscribe', function (req, res, next) {
 
@@ -58,20 +57,21 @@ router.post('/compsubscribe', function (req, res, next) {
             },
             body: body
         },
+
         function (error, response, body) {
             if (!error) {
                 if (response.statusCode > 400) {
                     // http level problem - didn't connect through to mailchimp
-                    res.send({ status: 'failed', reason: 'bad status code: ' + response.statusCode });
+                    res.send({status: 'failed', reason: 'bad status code: ' + response.statusCode});
+
                 } else {
+
                     var mailchimpResponse = JSON.parse(body);
 
                     if (mailchimpResponse.status >= 400) {
 
-                        // In these cases we do not get back the current member
-                        // data - just an 'existing member' message
-
-                        // Does this member/email already exist in mailchimp?
+                        // Does this member/email already exist in mailchimp? If so, we can
+                        // resubmit the request as an update operation
                         if (mailchimpResponse.status == 400 && mailchimpResponse.title == 'Member Exists') {
                             // update the answer
 
@@ -96,7 +96,7 @@ router.post('/compsubscribe', function (req, res, next) {
                                 }, function (error, response, body) {
                                     if (!error) {
                                         // s'all goooood hombre
-                                        res.send({ status: 'success' });
+                                        res.send({status: 'success'});
                                     } else {
                                         res.send({
                                             status: 'failed',
@@ -106,68 +106,13 @@ router.post('/compsubscribe', function (req, res, next) {
                                 });
 
                         } else {
-                            // generic error
-                            res.send({ status: 'failed', reason: 'mailchimp generic error: ' + response.statusCode });
+                            // must be a generic error
+                            res.send({status: 'failed', reason: 'mailchimp generic error: ' + response.statusCode});
                         }
 
                     } else {
                         // s'all goooood hombre
-                        res.send({ status: 'success' });
-                    }
-                }
-            } else {
-                res.send({ status: 'failed', reason: error });
-            }
-        }
-    );
-});
-
-
-
-// GET subscriptions/winner
-router.get('/winner', function (req, res, next) {
-    res.send('not yet implemented');
-});
-
-// POST subscriptions/xrconnectregister
-router.post('/xrconnectregister', function (req, res, next) {
-
-    // CORS Headers (client > gme app, not gme >  mailchimp)
-    // todo: SECURE THIS ON PRODUCTION!
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    // var listId = '05bed547f8';
-    var listId = 'a6f1694bb5';
-    var body = JSON.stringify({
-        'email_address': Math.random() + '-' + Math.random() + '@emailrand.com',
-        'status': 'subscribed',
-        'merge_fields': {
-            'MMERGE3': req.body.data
-        }
-    });
-
-    request(
-        {
-            method: 'POST',
-            url: mailchimpUrl + '/3.0/lists/' + listId + '/members/',
-            headers: {
-                'Authorization': 'apikey ' + apiKey,
-            },
-            body: body
-        },
-        function (error, response, body) {
-            if (!error) {
-                if (response.statusCode > 400) {
-                    // http level problem - didn't connect through to mailchimp
-                    res.send({status: 'failed', reason: 'bad status code: ' + response.statusCode});
-                } else {
-                    var mailchimpResponse = JSON.parse(body);
-                    if (mailchimpResponse.status >= 400) {
-                        res.send({status: 'fail', mailchimpresponse: mailchimpResponse, body: req.body.data});
-
-                    } else {
-                        // s'all goooood hombre
-                        res.send({status: 'success', mailchimpresponse: mailchimpResponse, echo: req.body.data});
+                        res.send({status: 'success'});
                     }
                 }
             } else {
@@ -175,6 +120,116 @@ router.post('/xrconnectregister', function (req, res, next) {
             }
         }
     );
+});
+
+
+// GET subscriptions/winner
+router.get('/winner', function (req, res, next) {
+    res.send('not yet implemented');
+});
+
+// POST subscriptions/xrconnectregister   USED BY XRS IOS/ANDROID APP
+router.post('/xrconnectregister', function (req, res, next) {
+
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+    try {
+
+        // MMERGE6 == serial number
+        // MMERGE7 == radio model
+
+        var bodyIncomingParsed = JSON.parse(req.body.data);
+
+        var body = JSON.stringify({
+            'email_address': Math.random() + '-' + Math.random() + '@emailrand.com',
+            'status': 'subscribed',
+            'merge_fields': {
+                'MMERGE6': bodyIncomingParsed.SerialNumber,
+                'MMERGE7': bodyIncomingParsed.RadioModel
+            }
+        });
+
+        request(
+            {
+                method: 'POST',
+                url: mailchimpUrl + '/3.0/lists/daf92284c5/members/',
+                headers: {
+                    'Authorization': 'apikey ' + apiKey,
+                },
+                body: body
+            },
+
+            function (error, response, body) {
+                if (!error) {
+                    if (response.statusCode > 400) {
+                        // http level problem - didn't connect through to mailchimp
+                        res.send({status: 'failed', reason: 'bad status code: ' + response.statusCode});
+
+                    } else {
+
+                        var mailchimpResponse = JSON.parse(body);
+
+                        if (mailchimpResponse.status >= 400) {
+
+                            // Does this member/email already exist in mailchimp? If so, we can
+                            // resubmit the request as an update operation
+                            if (mailchimpResponse.status == 400 && mailchimpResponse.title == 'Member Exists') {
+                                // update the answer
+
+                                var md5 = require('blueimp-md5');
+                                var subscriberHash = md5(req.body.email.toLowerCase());
+
+                                var bodyUpdate = JSON.stringify({
+                                    'merge_fields': {
+                                        'MMERGE6': bodyIncomingParsed.SerialNumber,
+                                        'MMERGE7': bodyIncomingParsed.RadioModel
+                                    }
+                                });
+
+                                request(
+                                    {
+                                        method: 'PATCH',
+                                        url: mailchimpUrl + '/3.0/lists/' + listId + '/members/' + subscriberHash,
+                                        headers: {
+                                            'Authorization': 'apikey ' + apiKey,
+                                        },
+                                        body: bodyUpdate
+                                    }, function (error, response, body) {
+                                        if (!error) {
+                                            // s'all goooood hombre
+                                            res.send({status: 'success'});
+                                        } else {
+                                            res.send({
+                                                status: 'failed',
+                                                reason: 'error when trying to update existing email record'
+                                            });
+                                        }
+                                    });
+
+                            } else {
+                                // must be a generic error
+                                res.send({status: 'failed', reason: 'mailchimp generic error: ' + response.statusCode});
+                            }
+
+                        } else {
+                            // s'all goooood hombre
+                            res.send({status: 'success'});
+                        }
+                    }
+                } else {
+                    res.send({status: 'failed', reason: error});
+                }
+            }
+
+
+
+        );
+
+
+    } catch (err) {
+        console.log('could not process Mailchimp request, on to SQL insert')
+    }
 
 
     // Also save to Azure SQL table
@@ -191,10 +246,19 @@ router.post('/xrconnectregister', function (req, res, next) {
         }
     }
 
+    var bodySql = JSON.stringify({
+        'email_address': Math.random() + '-' + Math.random() + '@emailrand.com',
+        'status': 'subscribed',
+        'merge_fields': {
+            'MMERGE3': req.body.data
+        }
+    });
+
+
     sql.connect(config).then(function () {
         // Query
         new sql.Request()
-            .query("insert into GMELocationLog (LogDateTime, LogStatus) values ('" + new Date().toISOString() + "','" + body + "')").then(function (recordset) {
+            .query("insert into GMELocationLog (LogDateTime, LogStatus) values ('" + new Date().toISOString() + "','" + bodySql + "')").then(function (recordset) {
             console.log('good news');
             console.log(recordset);
         }).catch(function (err) {
